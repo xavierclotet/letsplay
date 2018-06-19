206ac63e3987de824cab1a9c767b2a01e98b7262
Xavi Clotet <xavi.clotet@bankimia.com>
2018-06-19 08:54:29 +0200

    example form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,34 @@ import { AngularFirestore } from 'angularfire2/firestore';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  users;
-  constructor(private db: AngularFirestore) { }
+  users: Observable<any>;
+  loading: boolean;
+  form: FormGroup;
+  success: boolean;
+  constructor(private afs: AngularFirestore, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.users = this.db.collection('users').valueChanges();
+    this.users = this.afs.collection('users').valueChanges();
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+
+    })
+  }
+
+
+  async submitHandler() {
+    this.loading = true;
+    const formValue = this.form.value;
+    try {
+      await this.afs.collection('users').add(formValue);
+      this.success = true;
+    } catch(err) {
+      console.error(err);
+    }
+
+    this.loading = false;
+
   }
 
 
